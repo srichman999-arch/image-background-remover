38dd6f36d1399961f9e273000851c3803efd874f
srichman999-arch <srichman999@gmail.com>
2026-03-28 20:40:05 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
