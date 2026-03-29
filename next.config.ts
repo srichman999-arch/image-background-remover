5e8e7ff9da9fcb2060dd84d98f8949625e673990
srichman999-arch <srichman999@gmail.com>
2026-03-29 09:16:52 +0800

    修复配置文件冲突

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['129.226.219.251'],
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   output: "export",
   distDir: "out",
   images: {
     unoptimized: true,
   },
->>>>>>> a4f7fb3e4d3178473a9dc8610882478dff3c60e3
+  allowedDevOrigins: ['129.226.219.251'],
 };
 
 export default nextConfig;
